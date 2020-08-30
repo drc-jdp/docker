d940f3c4d18dfa9879d7b47c80989de3be7bef26
yqchenee <a6330610@gmail.com>
2020-08-30 23:05:32 +0800

    init

--- boot.sh
+++ boot.sh
@@ -2,4 +2,4 @@
 
 # runuser -l ga -c './actions-runner/config.sh --url https://github.com/yqchenee/WebProg2019 --token ALM2SOTTEZ2WLPKCTYJWYBC7JOWUI  --name gsh --unattended'
 # runuser -l ga -c './actions-runner/run.sh'&
-python3 ../http-server.py 8000 tyler:tt -D
+python3 ../http-server.py 8000 un:pswd -D
